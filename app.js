20b2d49421740f90a5300d279b078ae04121bbe0
Sushant Nilange <sushantnilange@gmail.com>
2020-07-01 22:11:06 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my Nodemon API!');
+});
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}`);
+});
